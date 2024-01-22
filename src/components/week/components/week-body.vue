bb19f7bf3d8c7889b9f5cb93da380c4cd6871e4d
abean <abeanz@163.com>
2024-01-22 19:07:13 +0800

    feat: week calendar开发

--- src/components/week/components/week-body.vue
+++ src/components/week/components/week-body.vue
@@ -1,16 +1,21 @@
 <template>
-  <div v-for="item of 7" :key="item" class="flex-1 position-relative" @mouseenter="mouseenter">
-    <div class="position-absolute top--1.6 left-0 right-0 h1.6" :class="{ 'b-r-solid b-r-1 b-r-#ccc': item !== 7 }"></div>
-    <TimeDivider :need-right-border="item !== 7" />
-    <DayTask v-for="data of mockData" :key="data.id" :data="data" class="w98%" />
+  <div v-for="(data1, index) of store.data" :key="index" class="flex-1 position-relative" @mouseenter="mouseenter">
+    <div class="position-absolute top--1.6 left-0 right-0 h1.6" :class="{ 'b-r-solid b-r-1 b-r-#ccc': index !== 7 }"></div>
+    <TimeDivider :need-right-border="index !== 7" />
+    <template v-for="(items, index) of groupSchedulesByOverlap(data1)" :key="index">
+      <template v-for="(item, i) of items" :key="item.id">
+        <DayTask :data="item" :style="{ width: `${100 / items.length}%`, left: `${i * (100 / items.length)}%` }" />
+      </template>
+    </template>
   </div>
 </template>
 <script setup lang="ts">
-import { useDay } from '@/hooks/useDay';
+import { useStore } from '@/hooks/useStore';
 import TimeDivider from '@/components/time-divider/time-divider.vue';
 import DayTask from '@/components/day/components/day-task.vue';
+import { groupSchedulesByOverlap } from '@/utils';
 
-const { mockData } = useDay();
+const { store } = useStore();
 
 const mouseenter = (e: Event) => {
   // console.log(e);
